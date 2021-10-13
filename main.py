c6354062144fe704d7b7c19a0e878f515cca11b5
Alexander <gwerty31102005@gmail.com>
2021-10-13 21:00:50 +0300

    tried dadata geocoding

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from dadata import Dadata
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+token = "83ee29b164712c40a3104fb10813bde0457e23f6"
+secret = "b8f8e51f7213f8f741899ad691c68fce1b16b1de"
+for i in range(9000):
+    ddt = Dadata(token, secret)
+    result = ddt.clean("address", "карламарксадом23северодвинск")
+    print(result)
