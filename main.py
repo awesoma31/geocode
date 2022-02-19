10ef9b9f1b4bffde9b96943170f134371b233a4d
Alexander <gwerty31102005@gmail.com>
2022-02-19 17:07:22 +0300

    0.6.5 refactored

--- main.py
+++ main.py
@@ -10,21 +10,34 @@ def transform_data(address):
     loc_pattern = r'\b(?:округ|окр|д|дер|деревня|г|гор|город|с|село|п|пос|поселок|проезд|рп|снт|СНТ)\b\.*\s*[а-яА-Я\-]+\d*\s*[а-яА-Я\-]*'
     street_pattern = r'\b(?:ул\.\sим\.s\проф|ул\.*\s*им|улица\sим|улица|ул|у|пер\.*\s*им|пер|переулок|наб\.\s*им|наб|набережная|набер|ш|шоссе|пр\.\sим|пр\-кт|пр|проспект|кв\-л|кв|квартал)\b\.*\s*[а-яА-Я\s\-]+\d*\.*\s*[а-яА-Я\-]+\d*\.*\s*[а-яА-Я\-]+\d*'
     building_pattern = r'\b(?:д|дом)\b\.*\s*[\d]+\s*\w{0,1}\b'
+    corps_pattern = r'\bк\d{1,}'
 
     loc = re.findall(loc_pattern, address)
     street = re.findall(street_pattern, address)
     building = re.findall(building_pattern, address)
-    return loc, street, building
+    corps = re.findall(corps_pattern, address)
+    return loc, street, building, corps
 
 
 def clear_loc(loc):
     """
-    Убирает ул., улица и т.д., оставляя только рабочую часть
+    Убирает населенный пункт, оставляя только рабочую часть
     """
     loc_sub_pattern = r'\b(?:округ|окр|д|дер|деревня|г|гор|город|с|село|п|пос|поселок|проезд|рп|снт|СНТ)\b\.*\s*'
 
     location = re.sub(loc_sub_pattern, r'', loc[-1])
     return location
+
+
+def clear_street(street):
+    """
+    Убирает ул улицу и т.д, оставляя только рабочую часть
+    """
+    street_sub_pattern = r'\b(?:ул\.\sим\.s\проф|ул\.*\s*им|улица\sим|улица|ул|у|пер\.*\s*им|пер|переулок|наб\.\s*им|наб|набережная|набер|ш|шоссе|пр\.\sим|пр\-кт|пр|проспект|кв\-л|кв|квартал)\b\.*\s*'
+
+    street = re.sub(street_sub_pattern, r'', street[0])
+    print(street, '!!!!!!!!!!!!')
+    return street
 
 
 def clear_building(building):
@@ -35,6 +48,35 @@ def clear_building(building):
 
     build = re.sub(building_sub_pattern, r'', building[0])
     return build
+
+
+def update_table(cn, lat, lon):
+    cur2 = connection.cursor()
+    cur2.execute(
+        f"""
+        UPDATE all_oks_b
+        SET latitude = {lat}, longitude = {lon}
+        WHERE cn = {"'" + cn + "'"}
+        """
+    )
+    cur2.close()
+    connection.commit()
+
+
+def get_string_to_geocode(loc, street, building, corps):
+    if len(street) != 0:
+        if len(corps) == 0:
+            geocode_string = f'{loc}, {street} {building}'
+        else:
+            geocode_string = f'{loc}, {street} {building} {corps[0]}'
+
+    else:
+        if len(corps) == 0:
+            geocode_string = f'{loc}, {building}'
+        else:
+            geocode_string = f'{loc}, {building} {corps[0]}'
+
+    return geocode_string
 
 
 geolocator = Nominatim(user_agent="user_agent")
@@ -50,20 +92,18 @@ try:
         host="localhost",
         port="5432"
     )
-    # connection.autocommit = True
 
     cur = connection.cursor()
 
     cur.execute(
         f"""
         SELECT address, cn
-        FROM all_oks_b
-        WHERE address SIMILAR TO '%(город|гор|г).*\s*Архангельск%'
-        ORDER BY id
+        FROM ar_oks
+        WHERE latitude IS NULL
         """
     )
-    data = True
 
+    data = True  # just to start process
     while data:
         count += 1
         print(count)
@@ -72,91 +112,50 @@ try:
             data = cur.fetchone()
             print(data)
         except psycopg2.ProgrammingError as e:
-            data = ('-', '-')
+            data = ('empty', 'empty')
 
         address = data[0]
         cn = data[1]
         print(f'Initial address is:\n\t{address}\n')
 
-        loc, street, building = transform_data(address)
-        print(f'Initial data:\n\t{loc}, {street}, {building}')
+        loc, street, building, corps = transform_data(address)
+        print(f'Initial data:\n\t{loc}, {street}, {building}, {corps}')
 
-        if len(loc) <= 0 or len(building) <= 0:
+        if len(loc) == 0 or len(building) == 0:
             print('-----')
             print('[GEOCODING ERROR] NOT ENOUGH DATA')
             print('_______')
-            pass
 
         else:
             if len(street) > 0:
                 loc = clear_loc(loc)
+                street = clear_street(street)
                 building = clear_building(building)
-                print(f'Workable data:\n\t{loc}, {street[0]} {building}\n')
-
-                location = geolocator.geocode(f'{loc}, {street[0]} {building}', timeout=None)
-
-                if location is None:
-                    print('[GEOCODING ERROR] Location type is None')
-                    print('_______')
-
-
-                else:
-                    lat = location.latitude
-                    lon = location.longitude
-                    print(lat, lon)
-                    print('-----')
-                    print('[GEOCODING SUCCESS]')
-                    print('_______')
-
-                    cur1 = connection.cursor()
-
-                    cur1.execute(
-                        f"""
-                        UPDATE all_oks_b
-                        SET latitude = {lat}, longitude = {lon}
-                        WHERE cn = {"'" + cn + "'"}
-                        """
-                    )
-
-                    cur1.close()
-
-                    connection.commit()
-                    success_count += 1
 
             else:
                 loc = clear_loc(loc)
                 building = clear_building(building)
-                print(f'Workable data:\n\t{loc}, {building}\n')
 
-                location = geolocator.geocode(f'{loc} {building}', timeout=None)
+            geocode_string = get_string_to_geocode(loc, street, building, corps)
+            print(f'Workable data:\n\t{geocode_string}')
 
-                if location is None:
-                    print(f'[GEOCODING ERROR] Location type is None')
-                    print('_______')
+            location = geolocator.geocode(geocode_string, timeout=None)
 
+            if location is None:
+                print('[GEOCODING ERROR] Location type is None')
+                print('_______')
 
-                else:
-                    lat = location.latitude
-                    lon = location.longitude
-                    print(lat, lon)
-                    print('-----')
-                    print('[GEOCODING SUCCESS]')
-                    print('_______')
+            else:
+                lat = location.latitude
+                lon = location.longitude
+                print(lat, lon)
+                print('-----')
+                print('[GEOCODING SUCCESS]')
+                print('_______')
 
-                    cur2 = connection.cursor()
+                update_table(cn, lat, lon)
 
-                    cur2.execute(
-                        f"""
-                        UPDATE all_oks_b
-                        SET latitude = {lat}, longitude = {lon}
-                        WHERE cn = {"'" + cn + "'"}
-                        """
-                    )
-
-                    cur2.close()
-
-                    connection.commit()
-                    success_count += 1
+                success_count += 1
 
     connection.commit()
     cur.close()
@@ -175,4 +174,7 @@ finally:
     if connection:
         connection.close()
         print("INFO: [Connection with PostgreSQL closed]")
-        print(f'INFO: Success issues {success_count} from {count} attempts')
+        print(
+            f'INFO: Success issues {success_count} from {count} attempts, percentage {round((success_count / count * 100), 1)}%')
+
+# average success 82%
